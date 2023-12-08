8d36d7f5959fe68cadcac15716299f9c5b11f736
Vijayt2001 <62590408+Vijayt2001@users.noreply.github.com>
2023-12-07 22:29:57 -0800

    login and sign up pages UI and aunthentication

---
+++ Music App - CSC 648/SignUpViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SignUpViewController.swift
+//  Music App - CSC 648
+//
+//  Created by VJ 2 on 12/7/23.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signupClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                print("Error")
+            }
+            else{
+                //Home screen
+                self.shouldPerformSegue(withIdentifier: "goToNext", sender: self)
+            }
+        }
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
